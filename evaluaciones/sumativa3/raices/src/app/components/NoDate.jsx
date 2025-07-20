ad585e0ce0d80c341a7723ddba580497ff1db0ad
gvmarcus <Marcogaetev@gmail.com>
2025-07-19 22:53:29 -0400

    reload

--- evaluaciones/sumativa3/raices/src/app/components/NoDate.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/NoData.jsx
-import React from 'react';
-
-export const NoData = () => (
-  <div className="text-center text-gray-500 text-lg mt-10">
-    No hay inscripciones disponibles en este momento.
-  </div>
-);
-
